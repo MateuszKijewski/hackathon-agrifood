db9e521e20d486905d547deca6a38b6534583332
Mateusz <mateuszkijewski2307@gmail.com>
2020-12-05 08:30:15 +0100

    Created first service

---
+++ Hackathon.AgriFood.Services/Interfaces/ICustomerService.cs
@@ -0,0 +1,21 @@
+﻿using Hackathon.AgriFood.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Hackathon.AgriFood.Services.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task AddCustomer(CustomerDto customerDto);
+        Task AddManyCustomers(IEnumerable<CustomerDto> customerDtos);
+        Task<CustomerDto> GetCustomer(Guid id);
+        Task<IEnumerable<CustomerDto>> GetAllCustomers();
+
+        Task UpdateCustomer(CustomerDto customerDto);
+        Task UpdateManyCustomers(IEnumerable<CustomerDto> customerDtos);
+        Task DeleteCustomer(CustomerDto customerDto);
+        Task DeleteManyCustomers(IEnumerable<CustomerDto> customerDtos);
+
+    }
+}
---
+++ Hackathon.AgriFood.Services/Services/CustomerService.cs
@@ -0,0 +1,89 @@
+﻿using Hackathon.AgriFood.Models.Converters.Interfaces;
+using Hackathon.AgriFood.Models.Dtos;
+using Hackathon.AgriFood.Models.Models;
+using Hackathon.AgriFood.Repositories.Providers;
+using Hackathon.AgriFood.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hackathon.AgriFood.Services.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly IRepositoryProvider _repositoryProvider;
+        private readonly IEntityConverter _entityConverter;
+
+        public CustomerService(IRepositoryProvider repositoryProvider,
+            IEntityConverter entityConverter)
+        {
+            _repositoryProvider = repositoryProvider;
+            _entityConverter = entityConverter;
+        }
+
+        public async Task AddCustomer(CustomerDto customerDto)
+        {
+            var customerRepository = _repositoryProvider.GetRepository<Customer>();
+            var customer = _entityConverter.GetModelFromDto(customerDto);
+
+            await customerRepository.Add(customer);
+        }
+
+        public async Task AddManyCustomers(IEnumerable<CustomerDto> customerDtos)
+        {
+            var customerRepository = _repositoryProvider.GetRepository<Customer>();
+            var customers = customerDtos.Select(x => _entityConverter.GetModelFromDto(x));
+
+            await customerRepository.AddRange(customers);
+        }
+
+        public async Task DeleteCustomer(CustomerDto customerDto)
+        {
+            var customerRepository = _repositoryProvider.GetRepository<Customer>();
+            var customer = _entityConverter.GetModelFromDto(customerDto);
+
+            await customerRepository.Delete(customer);
+        }
+
+        public async Task DeleteManyCustomers(IEnumerable<CustomerDto> customerDtos)
+        {
+            var customerRepository = _repositoryProvider.GetRepository<Customer>();
+            var customers = customerDtos.Select(x => _entityConverter.GetModelFromDto(x));
+
+            await customerRepository.DeleteRange(customers);
+        }
+
+        public async Task<IEnumerable<CustomerDto>> GetAllCustomers()
+        {
+            var customerRepository = _repositoryProvider.GetRepository<Customer>();
+            var customers = await customerRepository.GetAll();
+
+            return customers.Select(x => _entityConverter.GetDtoFromModel(x));
+        }
+
+        public async Task<CustomerDto> GetCustomer(Guid id)
+        {
+            var customerRepository = _repositoryProvider.GetRepository<Customer>();
+            var customer = await customerRepository.Get(id);
+
+            return _entityConverter.GetDtoFromModel(customer);
+        }
+
+        public async Task UpdateCustomer(CustomerDto customerDto)
+        {
+            var customerRepository = _repositoryProvider.GetRepository<Customer>();
+            var customer = _entityConverter.GetModelFromDto(customerDto);
+
+            await customerRepository.Update(customer);
+        }
+
+        public async Task UpdateManyCustomers(IEnumerable<CustomerDto> customerDtos)
+        {
+            var customerRepository = _repositoryProvider.GetRepository<Customer>();
+            var customers = customerDtos.Select(x => _entityConverter.GetModelFromDto(x));
+
+            await customerRepository.UpdateRange(customers);
+        }
+    }
+}
